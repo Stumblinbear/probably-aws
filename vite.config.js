27735f9d889350de21b927fa71786162b30e3dce
Stumblinbear <stumblinbear@gmail.com>
2021-11-27 08:46:08 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     Components({
